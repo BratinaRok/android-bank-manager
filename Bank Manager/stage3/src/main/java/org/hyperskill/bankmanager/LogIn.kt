1905dbaf6ba54082a013284eeaf7efa51055e008
Rok <bratinarok@gmail.com>
2022-11-13 22:39:06 +0100

    stage 3 update2

--- Bank Manager/stage3/src/main/java/org/hyperskill/bankmanager/LogIn.kt
+++ Bank Manager/stage3/src/main/java/org/hyperskill/bankmanager/LogIn.kt
@@ -4,15 +4,14 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.EditText
 import androidx.fragment.app.Fragment
-import org.hyperskill.bankmanager.R
+import androidx.navigation.fragment.findNavController
 import org.hyperskill.bankmanager.databinding.LogInBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
-class LogIn : Fragment() {
+ class LogIn : Fragment() {
 
 
     private var _binding: LogInBinding? = null
@@ -32,14 +31,19 @@ class LogIn : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.confirmCodeButton.setOnClickListener {
-            var codeEntered = view.findViewById<EditText>(R.id.securityCodeInput)
-            if ((activity as MainActivity).securityCodeCheck(view,codeEntered)) {
 
+        binding.confirmCodeButton.setOnClickListener {
+            if ((activity as MainActivity).securityCheck(view)) {
+                val objUserData = LogInUser()
+
+                if (objUserData.isUserNameIdentification && objUserData.isPasswordIdentification) {
+                    findNavController().navigate(org.hyperskill.bankmanager.R.id.action_SecondFragment_to_userMenu)
+                }
 
             }
         }
     }
+
 
 
     override fun onDestroyView() {
